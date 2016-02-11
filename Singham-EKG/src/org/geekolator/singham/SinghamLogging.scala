27a9576138b761fdb00b5b6922fec8bdb3bd7c69
ryerramilli <ram.yerramilli@gmail.com>
2016-02-10 19:24:57 -0800

    fix imports

--- Singham-EKG/src/org/geekolator/singham/SinghamLogging.scala
+++ Singham-EKG/src/org/geekolator/singham/SinghamLogging.scala
@@ -1,12 +1,10 @@
 package org.geekolator.singham
 
 import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
 
 trait SinghamLogging {
-  
-  import com.typesafe.scalalogging._
-  import org.slf4j.LoggerFactory
-  
+   
   val logger = Logger(LoggerFactory.getLogger(getClass))
   
 }
