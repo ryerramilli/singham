e24f45d6611846a5d556e48c7bdf5491fef5c5f2
ryerramilli <ramyerramilli@Rams-MacBook-Pro.local>
2016-03-25 12:18:31 -0700

    Display roadmap using svg instead of html
    It is more elegant, I think.

--- singham-webapp/src/webapp.js
+++ singham-webapp/src/webapp.js
@@ -5,12 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var singham_routes = require('./routes/singham');
-
-
 var app = express();
+
+//var routes = require('./routes/index');
+//app.use('/', routes);
+
+//var users = require('./routes/users');
+//app.use('/users', users);
+
+var singham_routes = require('./routes/singham');
+app.use('/', singham_routes);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,10 +27,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/s', singham_routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
